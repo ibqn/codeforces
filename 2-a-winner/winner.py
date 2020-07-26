4b27d7ca084a393a2c1eccc7d538464f0a38fcf4
ibqn <evgen.ibqn@gmail.com>
2020-07-26 09:13:40 +0200

    rewrite python version of 2 a. winner

--- 2-a-winner/winner.py
+++ 2-a-winner/winner.py
@@ -1,32 +1,23 @@
-from itertools import takewhile
-
 n = int(input())
 
-players = {}
+scores = {}
+history = []
 
 for i in range(n):
     name, points = input().split()
 
-    data = players.get(name, {'score': 0, 'points': []})
-    data['score'] += int(points)
-    data['points'].append((data['score'], i + 1))
+    scores[name] = scores.get(name, 0) + int(points)
+    history.append((name, scores[name]))
 
-    players[name] = data
+max_score = max(scores.values())
 
-winners = sorted(players.items(), key=lambda item: item[1]['score'], reverse=True)
+gen = (
+    name for (name, score) in history
+    if scores[name] == max_score and score >= max_score
+)
+winner = next(gen)
 
-top = winners[0]
-
-top_shared = [p for p in takewhile(lambda p: p[1]['score'] == top[1]['score'], winners)]
-
-top_list = []
-for top in top_shared:
-    name, hist = top
-    min_round = min([ p[1] for p in hist['points'] if p[0] >= hist['score'] ])
-    top_list.append((name, min_round))
-
-winner = sorted(top_list, key=lambda item: item[1]).pop(0)
-print(winner[0])
+print(winner)
 
 
 
