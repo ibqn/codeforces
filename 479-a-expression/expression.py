d07716d02359025bf58c9cd0e2b85f39d73747dd
ibqn <evgen.ibqn@gmail.com>
2020-08-01 12:44:12 +0200

    iprove python code of 479 a. expression

--- 479-a-expression/expression.py
+++ 479-a-expression/expression.py
@@ -1,30 +1,11 @@
-from functools import reduce
+a, b, c = [int(input()) for _ in range(3)]
 
-numbers = [int(input()) for _ in range(3)]
+operations = [
+    a + b + c,
+    a * b * c,
+    (a + b) * c,
+    a * b + c,
+    a * (b + c),
+]
 
-add = lambda num: sum(num)
-mult = lambda num: reduce(lambda x, y: x*y, num)
-
-def sum_mult_br(num):
-    a, b, c = num
-    return (a + b) * c
-
-def sum_mult(num):
-    a, b, c = num
-    return a + b * c
-
-def mult_sum(num):
-    a, b, c = num
-    return a * b + c
-
-def mult_sum_br(num):
-    a, b, c = num
-    return a * (b + c)
-
-operations = [add, mult, sum_mult, sum_mult_br, mult_sum, mult_sum_br]
-
-result = 0
-for op in operations:
-    result = max(result, op(numbers))
-
-print(result)
+print(max(operations))
