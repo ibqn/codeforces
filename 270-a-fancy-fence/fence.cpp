71744c80b30bbe89f4e67b7e89d99e864c3625c9
ibqn <evgen.ibqn@gmail.com>
2020-08-11 23:51:35 +0200

    add a cpp version of 270 a. fancy fence

---
+++ 270-a-fancy-fence/fence.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        int a;
+        cin >> a;
+
+        cout << ((360 % (180 - a) == 0) ? "YES" : "NO") << endl;
+    }
+}
