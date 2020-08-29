6f60884228e84f2a8a230d5cb56fcf90428991d8
ibqn <evgen.ibqn@gmail.com>
2020-08-29 18:12:54 +0200

    remove attempts for 3 b. lorry

--- 3-b-lorry/lorry.py
+++ 3-b-lorry/lorry.py
@@ -1,54 +1,47 @@
-n, c = map(int, input().split())
+n, capacity = map(int, input().split())
 
-one, two = 1, 2
-
-
-a, b = [], []
+kayaks, catamarans = [], []
 
 for i in range(n):
-    veh_type, veh_cap = map(int, input().split())
-    l = a if veh_type == 1 else b
-    l.append(( veh_cap, i + 1 ))
+    vehicle_type, vehicle_capacity = map(int, input().split())
+    l = kayaks if vehicle_type == 1 else catamarans
+    l.append(( vehicle_capacity, i + 1 ))
 
-a.sort(key=lambda t: t[0], reverse=True)
-b.sort(key=lambda t: t[0], reverse=True)
+kayaks.sort(key=lambda item: item[0], reverse=True)
+catamarans.sort(key=lambda item: item[0], reverse=True)
 
-print('ones', a, 'twos', b)
+# print('kayaks', kayaks, 'catamarans', catamarans)
+
+max_capacity = 0
+kayaks_cap = [0]
+catamarans_cap = [0]
+
+for i in range(min(capacity, len(kayaks))):
+    kayaks_cap.append(kayaks_cap[i] + kayaks[i][0])
+
+for i in range(min(capacity // 2, len(catamarans))):
+    catamarans_cap.append(catamarans_cap[i] + catamarans[i][0])
+
+# print('kayaks caps', kayaks_cap, 'catamarans caps', catamarans_cap)
+
+kayaks_idx = 0
+catamarans_idx = 0
+for i, k in enumerate(kayaks_cap):
+    if i > capacity:
+        break
+
+    idx = min((capacity - i) // 2, len(catamarans_cap) - 1)
+    if (k + catamarans_cap[idx] > max_capacity):
+        max_capacity = k + catamarans_cap[idx]
+        kayaks_idx = i
+        catamarans_idx = idx
+
+print(max_capacity)
+
+for i in range(kayaks_idx):
+    print(kayaks[i][1], end=' ')
+
+for i in range(catamarans_idx):
+    print(catamarans[i][1], end=' ')
 
 
-def idx(l, i):
-    try:
-        return l[i][0]
-    except IndexError:
-        return 0
-
-l = []
-m = n
-while m > 0:
-    if idx(a, 0) > idx(b, 0):
-        l.append((one, *a.pop(0)))
-        m-=1
-    elif idx(a, 0) + idx(a, 1) > idx(b, 0):
-        l.append((one, *a.pop(0)))
-        l.append((one, *a.pop(0)))
-        m-=2
-    else:
-        l.append((two, *b.pop(0)))
-        m-=1
-
-print(l)
-
-tot_cap = 0
-tot_typ = []
-while c > 0 and n > 0:
-    veh_type, cap, idx = l.pop(0)
-    # print('type', veh_type, "cap", cap)
-
-    if c >= veh_type:
-        c -= veh_type
-        tot_cap += cap
-        tot_typ.append(idx)
-    n -= 1
-
-print(tot_cap)
-print(" ".join(tot_typ))
