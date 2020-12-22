ad6de738276021c36eea14d81d65e9a2054dbe95
ibqn <evgen.ibqn@gmail.com>
2020-12-23 00:56:04 +0100

    add 1370 a. maximum gcd

---
+++ 1370-a-maximum-gcd/gcd.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var (
+		t, n int
+	)
+
+	in := bufio.NewReader(os.Stdin)
+	fmt.Fscan(in, &t)
+
+	for i := 0; i < t; i++ {
+		fmt.Fscan(in, &n)
+		fmt.Println(n / 2)
+	}
+}
