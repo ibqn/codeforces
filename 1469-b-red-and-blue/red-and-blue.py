806c46d682ade55bfafdd7f8ad5a79218980d55e
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-04-06 09:30:16 +0200

    refactor: simplify prefix sum calculation and remove unused code

--- 1469-b-red-and-blue/red-and-blue.py
+++ 1469-b-red-and-blue/red-and-blue.py
@@ -1,21 +1,25 @@
+from itertools import accumulate
+
+
 n = int(input())
 
 
 def prefix_sum():
     an = int(input())
-    a = list(map(int, input().split()))
+    a = map(int, input().split())
 
-    pa = []
-    s = 0
-    for i in range(an):
-        s += a[i]
-        pa.append(s)
+    # pa = [0]
+    # s = 0
+    # for i in range(an):
+    #     s += a[i]
+    #     pa.append(s)
 
-    return pa
+    # return pa
+    return accumulate(a, initial=0)
 
 
 for _ in range(n):
     pa = prefix_sum()
     pb = prefix_sum()
 
-    print(max(0, *pa) + max(0, *pb))
+    print(max(pa) + max(pb))
